5c906db87135444bad35a0e2ec2a448066dcc76e
Daniel Linhares Lim-Apo <634751@pcdf.gov.br>
2023-06-19 17:38:28 -0300

    useState implemented in ExpenseItem

--- src/reactcompleteguide/src/components/Expenses/ExpenseItem.js
+++ src/reactcompleteguide/src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState }  from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+
+const ExpenseItem = (props) => {
+
   //   const expenseDate = new Date(2021, 2, 28);
   //   const expenseTitle = "Car Insurance";
   //   const exepenseAmount = 294.67;
+
+  const [title, setTitle] =   useState(props.title);
+
+  // let title = props.title;
+
+  const clickHandler = () => {
+    // title = 'Updated!';
+    setTitle('Updated!');
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}> Change Title</button>
     </Card>
   );
 }
